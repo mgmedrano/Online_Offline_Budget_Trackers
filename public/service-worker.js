fc3d9c04f5091176599502018c6f9ddb887b0725
Michael Medrano <mgmedrano@outlook.com>
2021-10-19 17:25:14 -0700

    Updated service-worker code. Reroute package.json

---
+++ public/service-worker.js
@@ -0,0 +1,64 @@
+// List of files to cache root, index, icons, and style
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "styles.css"
+  ];
+  
+  const STATIC_CACHE = "static-cache-v1";
+  const RUNTIME_CACHE = "runtime-cache";
+  
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          // return array of cache names that are old to delete
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+        })
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  
+//   self.addEventListener("fetch", event => {
+//     if (
+//       event.request.method !== "GET" ||
+//       !event.request.url.startsWith(self.location.origin)
+//     ) {
+//       event.respondWith(fetch(event.request));
+//       return;
+//     }
+    
+        return caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    
