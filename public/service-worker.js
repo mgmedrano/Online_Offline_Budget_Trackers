7f653e93966e129493ff49826637ce5a1d5df3ac
Michael Medrano <mgmedrano@outlook.com>
2021-10-20 20:10:12 -0700

    Added db.js code for indexedDB

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,11 +5,12 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "styles.css"
+    "styles.css",
+    "db.js"
   ];
   
-  const STATIC_CACHE = "static-cache-v1";
-  const RUNTIME_CACHE = "runtime-cache";
+  const STATIC_CACHE = "static-cache-v2";
+  const RUNTIME_CACHE = "budget-cache";
   
   self.addEventListener("install", event => {
     event.waitUntil(
@@ -28,9 +29,7 @@ const FILES_TO_CACHE = [
         .keys()
         .then(cacheNames => {
           // return array of cache names that are old to delete
-          return cacheNames.filter(
-            cacheName => !currentCaches.includes(cacheName)
-          );
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
         })
         .then(cachesToDelete => {
           return Promise.all(
@@ -44,14 +43,43 @@ const FILES_TO_CACHE = [
   });
   
   
-//   self.addEventListener("fetch", event => {
-//     if (
-//       event.request.method !== "GET" ||
-//       !event.request.url.startsWith(self.location.origin)
-//     ) {
-//       event.respondWith(fetch(event.request));
-//       return;
-//     }
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+
+  // self.addEventListener("fetch", event => {
+  //   if (
+  //     event.request.method !== "GET" ||
+  //     !event.request.url.startsWith(self.location.origin)
+  //   ) {
+  //     event.respondWith(fetch(event.request));
+  //     return;
+  //   }
+
+    if (event.request.url.includes("/api/transaction")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
     
         return caches.open(RUNTIME_CACHE).then(cache => {
           return fetch(event.request).then(response => {
@@ -61,4 +89,5 @@ const FILES_TO_CACHE = [
           });
         });
       })
-    
+    );
+  });
