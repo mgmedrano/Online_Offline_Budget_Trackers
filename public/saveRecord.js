d116778d2ed37e48df9fe828fcdbf8fe6cf6e59d
Michael Medrano <mgmedrano@outlook.com>
2021-10-21 18:29:37 -0700

    updated server

---
+++ public/saveRecord.js
@@ -0,0 +1,10 @@
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+
+  // add record to your store with add method.
+  store.add(record);
+}
