ea00098dc468d0e37fd240c4bdf5043858aa1e0f
Michael Medrano <mgmedrano@outlook.com>
2021-10-21 22:14:55 -0700

    Updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,12 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/index.js",
+    "db.js",
+    "manifest.webmanifest"
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "styles.css",
-    "db.js"
+    "../routes/api.js",
   ];
   
   const STATIC_CACHE = "static-cache-v2";
